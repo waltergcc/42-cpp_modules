72161d1b4473b1621038699ab05c8c22ba805c6b
wcorrea- <waltergccruz@gmail.com>
2023-07-13 18:37:40 +0100

    add m02e01

--- module_02/ex01/Fixed.cpp
+++ module_02/ex01/Fixed.cpp
@@ -6,7 +6,7 @@
 /*   By: wcorrea- <wcorrea-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 16:46:57 by wcorrea-          #+#    #+#             */
-/*   Updated: 2023/07/13 17:54:36 by wcorrea-         ###   ########.fr       */
+/*   Updated: 2023/07/13 18:29:20 by wcorrea-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,25 +16,34 @@ Fixed::Fixed()
 {
 	std::cout << "Default constructor called" << std::endl;
 	this->_value = 0;
-	return ;
 }
 
-Fixed::Fixed(Fixed &copy)
+Fixed::Fixed(Fixed const &copy)
 {
 	std::cout << "Copy constructor called" << std::endl;
 	*this = copy;
-	return ;
+}
+
+Fixed::Fixed(int const value)
+{
+	std::cout << "Int constructor called" << std::endl;
+	this->_value = value * (1 << Fixed::_bits);
+}
+
+Fixed::Fixed(float const value)
+{
+	std::cout << "Float constructor called" << std::endl;
+	this->_value = value * (1 << Fixed::_bits);
 }
 
 Fixed::~Fixed()
 {
 	std::cout << "Destructor called" << std::endl;
-	return ;
 }
 
 Fixed &Fixed::operator=(Fixed const &copy)
 {
-	std::cout << "Assignation operator called" << std::endl;
+	std::cout << "Copy assigment operator called" << std::endl;
 	if (this == &copy)
 		return (*this);
 	this->_value = copy.getRawBits();
@@ -43,11 +52,26 @@ Fixed &Fixed::operator=(Fixed const &copy)
 
 int	Fixed::getRawBits(void) const
 {
-	std::cout << "getRawBits member function called" << std::endl;
 	return (this->_value);
 }
 
 void Fixed::setRawBits(int const raw)
 {
 	this->_value = raw;
+}
+
+int	Fixed::toInt(void) const
+{
+	return (this->_value / (1 << Fixed::_bits));
+}
+
+float Fixed::toFloat(void) const
+{
+	return ((float)this->_value / (float)(1 << Fixed::_bits));
+}
+
+std::ostream &operator<<(std::ostream &out, Fixed const &copy)
+{
+	out << copy.toFloat();
+	return (out);
 }
--- module_02/ex01/Fixed.hpp
+++ module_02/ex01/Fixed.hpp
@@ -6,7 +6,7 @@
 /*   By: wcorrea- <wcorrea-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 16:42:37 by wcorrea-          #+#    #+#             */
-/*   Updated: 2023/07/13 17:58:53 by wcorrea-         ###   ########.fr       */
+/*   Updated: 2023/07/13 18:36:13 by wcorrea-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,21 +14,36 @@
 # define FIXED_HPP
 
 #include <iostream>
+#include <cmath>
+
 class Fixed
 {
 	private:
+		// Variables
 		int					_value;
-		static const int	_bits = 8;
+		static const int	_bits = 16;
 			
 	public:
+		// Constructors
 		Fixed();
-		Fixed(Fixed &copy);
+		Fixed(Fixed const &copy);
+		Fixed(int const value);
+		Fixed(float const value);
 		~Fixed();
-
+		
+		// Overload
 		Fixed &operator=(Fixed const &copy);
-
+		
+		// Getters and Setters
 		int	getRawBits(void) const;
 		void setRawBits(int const raw);
+		
+		// Member functions
+		float	toFloat(void) const;
+		int		toInt(void) const;
+
 };
+
+std::ostream &operator<<(std::ostream &out, Fixed const &copy);
 
 #endif
