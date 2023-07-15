e0b4ff8395081bef460f541b2450c0006eb50b1a
wcorrea- <waltergccruz@gmail.com>
2023-07-15 11:36:54 +0100

    change test layout in m02e02

--- module_02/ex02/main.cpp
+++ module_02/ex02/main.cpp
@@ -6,7 +6,7 @@
 /*   By: wcorrea- <wcorrea-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/13 16:37:29 by wcorrea-          #+#    #+#             */
-/*   Updated: 2023/07/14 04:49:20 by wcorrea-         ###   ########.fr       */
+/*   Updated: 2023/07/15 11:35:56 by wcorrea-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -63,53 +63,49 @@ int	main(void)
     Fixed diff = b - c;
     Fixed prod = b * c;
     Fixed div = b / c;
-	std::cout << "Valor de b: " << b << std::endl;
-	std::cout << "Valor de c: " << c << std::endl;
-	std::cout << "b + c: " << sum << std::endl;
-	std::cout << "b - c: " << diff << std::endl;
-	std::cout << "b * c: " << prod << std::endl;
-	std::cout << "b / c: " << div << std::endl;
+	std::cout << "b = " << b << std::endl;
+	std::cout << "c = " << c << std::endl << std::endl;
+
+	std::cout << b << " + " << c << ": " << sum << std::endl;
+	std::cout << b << " - " << c << ": " << diff << std::endl;
+	std::cout << b << " * " << c << ": " << prod << std::endl;
+	std::cout << b << " / " << c << ": " << div << std::endl;
 	pressEnter();
 	
 	titleHeader("COMPARISON OPERATORS");
-	std::cout << "Valor de b: " << b << std::endl;
-	std::cout << "Valor de c: " << c << std::endl;
+	std::cout << "b = " << b << std::endl;
+	std::cout << "c = " << c << std::endl << std::endl;
     bool greater = b > c;
     bool less = b < c;
     bool greaterOrEqual = b >= c;
     bool lessOrEqual = b <= c;
     bool equal = b == c;
     bool notEqual = b != c;
-	std::cout << "b > c: " << greater << std::endl;
-	std::cout << "b < c: " << less << std::endl;
-	std::cout << "b >= c: " << greaterOrEqual << std::endl;
-	std::cout << "b <= c: " << lessOrEqual << std::endl;
-	std::cout << "b == c: " << equal << std::endl;
-	std::cout << "b != c: " << notEqual << std::endl;
+	std::cout << b << " > " << c << ": " << greater << std::endl;
+	std::cout << b << " < " << c << ": " << less << std::endl;
+	std::cout << b << " >= " << c << ": " << greaterOrEqual << std::endl;
+	std::cout << b << " <= " << c << ": " << lessOrEqual << std::endl;
+	std::cout << b << " == " << c << ": " << equal << std::endl;
+	std::cout << b << " != " << c << ": " << notEqual << std::endl;
 	pressEnter();
 
 	titleHeader("INCREMENT OPERATORS");
     Fixed e = a++;
     Fixed f = c--;
-	std::cout << "Valor de a: " << a << std::endl;
-	std::cout << "Valor de e: " << e << std::endl;
-	std::cout << "Valor de c: " << c << std::endl;
-	std::cout << "Valor de f: " << f << std::endl;
+	std::cout << "a = " << a << std::endl;
+	std::cout << "c = " << c << std::endl << std::endl;
+	std::cout << "e = a++\ne = " << e << std::endl;
+	std::cout << "f = c--\nf = " << f << std::endl;
 	pressEnter();
 
 	titleHeader("MIN AND MAX");
     Fixed minResult = Fixed::min(a, b);
     Fixed maxResult = Fixed::max(a, b);
-	std::cout << "Valor de a: " << a << std::endl;
-	std::cout << "Valor de b: " << b << std::endl;
-	std::cout << "Min: " << minResult << std::endl;
-	std::cout << "Max: " << maxResult << std::endl;
-	pressEnter();
-
-	titleHeader("OVERLOADING OPERATORS");
-    std::cout << "Valor de a: " << a << std::endl;
-    std::cout << "Valor de b: " << b << std::endl;
-    std::cout << "Valor de c: " << c << std::endl;
+	
+	std::cout << "a = " << a << std::endl;
+	std::cout << "b = " << b << std::endl << std::endl;
+	std::cout << "Min between " << a << " and " << b << " is " << minResult << std::endl;
+	std::cout << "Max between " << a << " and " << b << " is " << maxResult << std::endl;
 	pressEnter();
 
     return 0;
